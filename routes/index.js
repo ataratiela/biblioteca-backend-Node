4dbcbb3dc25f71ae8589a550450a13fab6c30391
Alex <alejalonta@gmail.com>
2018-07-27 14:44:16 +0200

    Peticiones HTTP-GET-PUT-POST-DELETE

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use('/libros', require('./libro'));
 
 module.exports = app;
